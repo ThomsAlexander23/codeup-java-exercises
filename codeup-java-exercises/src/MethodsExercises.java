3d94c39c98e0ba4e93e2188f53bcb6a1bf06a425
Alex <thoms.alexander23@gmail.com>
2021-04-27 14:35:44 -0400

    Initial Commit: finished required for exercise 1

---
+++ codeup-java-exercises/src/MethodsExercises.java
@@ -0,0 +1,38 @@
+public class MethodsExercises {
+    public static void main(String[] args) {
+        System.out.println(addMethod(3, 5));
+        System.out.println(subtractMethod(3, 5));
+        System.out.println(multiplyMethod(3, 5));
+        System.out.println(divideMethod(3, 5));
+        System.out.println(modulusMethod(3,1));
+        System.out.println(modulusMethod(3,0));
+    }
+
+    public static long addMethod(long x, long y) {
+        return x + y;
+    }
+
+    public static long subtractMethod(long x, long y) {
+        return x - y;
+    }
+
+    public static long multiplyMethod(long x, long y) {
+        return x * y;
+    }
+
+    public static long divideMethod(long x, long y) {
+        return x / y;
+    }
+
+    public static long modulusMethod(long x, long y) {
+        if (y != 0) {
+            return x % y;
+        }
+        long z=0;
+        return z;
+    }
+
+    public static long multiplyMethod(long x, long y){
+        for ()
+    }
+}
