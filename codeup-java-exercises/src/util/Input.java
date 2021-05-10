caa1f48cbf09ea922eda88c4905b736f0b6c10c1
Alex <thoms.alexander23@gmail.com>
2021-05-10 12:54:55 -0400

    Feat: refactored to throw exception if type conflict, then resolve using try catch with default to Zero

--- codeup-java-exercises/src/util/Input.java
+++ codeup-java-exercises/src/util/Input.java
@@ -5,44 +5,58 @@ import java.lang.String;
 public class Input {
     private static Scanner scanner;
 
-    static String getString(){
-        System.out.println("Please Enter Text");
+    public static String getString(){
+        System.out.println("Please Enter Text\n");
         scanner = new Scanner(System.in);
         return scanner.nextLine();
     }
 
-    static boolean yesNo(){
-        System.out.println("Yes or No?");
+   public static boolean yesNo(){
+        System.out.println("Continue? [Y/N]");
         scanner = new Scanner(System.in);
         String input = scanner.nextLine();
        return input.contains("y");
     }
 
-    static int getInt(){
+    public static int getInt(){
         System.out.println("Please Enter a whole number");
         scanner = new Scanner(System.in);
-        int input = scanner.nextInt();
+        int input;
+//        int input = scanner.nextInt();
+        try {
+            input = Integer.valueOf(getString());
+        } catch (NumberFormatException e){
+            System.out.print("Type conversion error, variable defaulted to Zero.\n");
+            input = 0;
+        }
         return input;
     }
 
-    static int getInt(int min, int max){
-        System.out.printf("Please enter a whole number between %s and %s", min, max);
+    public static int getInt(int min, int max){
+        System.out.printf("Please enter a whole number between %s and %s\n", min, max);
         scanner = new Scanner(System.in);
         int input = scanner.nextInt();
         return (input <= max && input >= min)? input : getInt(min,max);
     }
 
-    static double getDouble(){
-        System.out.printf("Please enter a number");
+    public static double getDouble(){
+        System.out.println("Please enter a number");
         scanner = new Scanner(System.in);
-        double input = scanner.nextDouble();
+//        double input = scanner.nextDouble();
+        double input;
+        try {
+            input = Double.valueOf(getString());
+        } catch (NumberFormatException e){
+            System.out.print("Type conversion error, variable defaulted to Zero.\n");
+            input = 0.00;
+        }
         return input;
     }
 
-    static double getDouble(double min, double max){
+    public static double getDouble(double min, double max) {
         System.out.printf("Please enter a number between %.0f and %.0f", min, max);
         scanner = new Scanner(System.in);
         double input = scanner.nextDouble();
-        return (input <= max && input >= min)? input : getDouble(min,max);
+        return (input <= max && input >= min) ? input : getDouble(min, max);
     }
 }
